5c996350e7257b1b3fa552f53677dd1b38933d60
David García <wlogz@hotmail.com>
2023-10-21 18:06:40 -0500

    Creando modelo empleado

--- backempleados/src/main/java/com/altunodev/backempleados/modelo/Empleado.java
+++ backempleados/src/main/java/com/altunodev/backempleados/modelo/Empleado.java
@@ -1,4 +1,64 @@
 package com.altunodev.backempleados.modelo;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "empleados")
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    @Column(unique = true, nullable = false)
+    private String correo;
+
+//    Constructor Vacío
+    public Empleado() {
+    }
+
+    public Empleado(Long id, String nombre, String apellido, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+//    Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
